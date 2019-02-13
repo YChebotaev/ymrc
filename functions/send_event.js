1b63d638be123ae7276ecbaf1da6a6af8af55cda
Yury Chebotaev <yury.79120345101@gmail.com>
2019-02-13 16:17:50 +0500

    Add 3rd argument to pusher.trigger

--- functions/send_event.js
+++ functions/send_event.js
@@ -5,14 +5,14 @@ var pusher = new Pusher({
   key: "3f43dc4d450902026e31",
   secret: "34f60b0ed788e14f60d3",
   cluster: "eu",
-  useTLS: true
+  encrypted: true
 });
 
 exports.handler = function(event, context, callback) {
   const { method, params } = JSON.parse(event.body);
   const [channelId] = params;
 
-  pusher.trigger(`RCID-${channelId}`, method);
+  pusher.trigger(`RCID-${channelId}`, method, {});
 
   callback(null, {
     statusCode: 200,
